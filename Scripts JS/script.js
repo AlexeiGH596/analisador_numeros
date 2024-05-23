148b200c05da16393ddf758025b38c554fe4817b
AlexGH596 <alexgithub596@gmail.com>
2024-05-23 19:56:41 -0300

    Bug Js resolvido

--- Scripts JS/script.js
+++ Scripts JS/script.js
@@ -3,17 +3,17 @@ document.addEventListener(`DOMContentLoaded`, function () {
     let botao_adicionar = document.getElementsByClassName(`botao`)[0]
     let lista = document.getElementsByClassName(`lista`)[0]
     let botao_finalizar = document.getElementsByClassName(`botao`)[1]
-    let saida0 = document.getElementsByTagName(`p`)[0]
     let saida1 = document.getElementsByTagName(`p`)[1]
     let saida2 = document.getElementsByTagName(`p`)[2]
     let saida3 = document.getElementsByTagName(`p`)[3]
     let saida4 = document.getElementsByTagName(`p`)[4]
     let saida5 = document.getElementsByTagName(`p`)[5]
     let saida6 = document.getElementsByTagName(`p`)[6]
+    let saida7 = document.getElementsByTagName(`p`)[7]
     let vetor = []
 
     botao_adicionar.addEventListener(`click`, adicionar)
     function adicionar() {
-        saida0.innerHTML = `Carro`;
+        
     }
 })
