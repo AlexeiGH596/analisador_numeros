82ad8ea555420d6eb2ad159da7208ac2275b51bd
AlexGH596 <alexgithub596@gmail.com>
2024-05-24 11:32:41 -0300

    Responsividade ajustada

--- Scripts JS/script.js
+++ Scripts JS/script.js
@@ -4,10 +4,36 @@ document.addEventListener(`DOMContentLoaded`, function () {
     let lista = document.getElementsByClassName(`lista`)[0]
     let botao_finalizar = document.getElementsByClassName(`botao`)[1]
     let saida = document.getElementsByTagName(`section`)[0]
-    let vetor = []
+    let vetor = Number([])
 
     botao_adicionar.addEventListener(`click`, adicionar)
     function adicionar() {
+        if (numero.value >= 1 && numero.value <= 100) {
+            alert(`Tudo ok`)
+        } else {
+            alert(`Valor inválido`);
+        }
+
+        /*         if (isNumero(numero.value) && ! inLista(numero.value)) {
+                    alert(`Tudo ok`);
+                } else {
+                    alert(`Valor inválido`);
+                }
         
+                function isNumero(n) {
+                    if (numero >= 1 && numero <= 100) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+        
+                function inLista(n) {
+                    if (lista.indexOf(n) != -1) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } */
     }
 })
