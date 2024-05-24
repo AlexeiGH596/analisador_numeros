5d8f6f298b040f4001648fb181f5c37ac920f5a3
AlexGH596 <alexgithub596@gmail.com>
2024-05-24 10:01:55 -0300

    Js mais eficiente

--- Scripts JS/script.js
+++ Scripts JS/script.js
@@ -3,13 +3,7 @@ document.addEventListener(`DOMContentLoaded`, function () {
     let botao_adicionar = document.getElementsByClassName(`botao`)[0]
     let lista = document.getElementsByClassName(`lista`)[0]
     let botao_finalizar = document.getElementsByClassName(`botao`)[1]
-    let saida1 = document.getElementsByTagName(`p`)[1]
-    let saida2 = document.getElementsByTagName(`p`)[2]
-    let saida3 = document.getElementsByTagName(`p`)[3]
-    let saida4 = document.getElementsByTagName(`p`)[4]
-    let saida5 = document.getElementsByTagName(`p`)[5]
-    let saida6 = document.getElementsByTagName(`p`)[6]
-    let saida7 = document.getElementsByTagName(`p`)[7]
+    let saida = document.getElementsByTagName(`section`)[0]
     let vetor = []
 
     botao_adicionar.addEventListener(`click`, adicionar)
